c8e590b014c656a8e0a7c5f0eac3710a50d2e399
manucarbonell@github.com <manucarbonell@github.com>
2017-09-04 16:23:36 +0200

    Normalize image size

--- datasets/EsposallesCompetition.py
+++ datasets/EsposallesCompetition.py
@@ -23,6 +23,11 @@ batch_size=config.batch_size
 im_height=config.im_height
 im_width=config.im_width
 
+def PIL2array(img):
+    return np.array(img.getdata(),
+                    np.uint8).reshape(img.size[1], img.size[0], 3)
+
+
 class EsposallesDataset():
     def __init__(self,BaseDir='/home/ntoledo/datasets/OfficialEsposalles',cvset='train',level='word'):
         self.BaseDir=BaseDir
@@ -43,6 +48,23 @@ class EsposallesDataset():
         self.w_id=-1
         self.epoch=0
         self.numberSamples=len(self.labels.keys())
+
+    def readNormalizedImage(self,imageid):
+        v=imageid.split('_');
+        filename=self.DataDir+'/'+'_'.join(v[0:2])+'/words/'+imageid+".png"
+        #img=1.-cv2.imread(filename,cv2.IMREAD_GRAYSCALE)/255.
+        im=Image.open(filename)
+
+        #Paste image in fixed size background
+        maxsize = (config.im_height,config.im_width)
+        im.thumbnail(maxsize)
+        background=Image.fromarray(np.zeros(maxsize))
+        background.paste(im)
+        im=np.array(background)
+
+        return im
+        #return np.pad(img,(max(0,30-height),max(0,30-width)),'constant')
+
     def generate_previous_labels_and_regdict(self):
         l=self.labels.keys()
         l=sorted(l,key=natural_key)
@@ -68,14 +90,7 @@ class EsposallesDataset():
     def get_example(self):
         #minx=30
         #miny=30
-        def readNormalizedImage(imageid):
-            v=imageid.split('_');
-            filename=self.DataDir+'/'+'_'.join(v[0:2])+'/words/'+imageid+".png"
-
-            img=1.-cv2.imread(filename,cv2.IMREAD_GRAYSCALE)/255.
-            height,width=np.shape(img)
-            return np.pad(img,(max(0,30-height),max(0,30-width)),'constant')
-            #return img
+        #return img
         #while True:
         if self.level=='word':
             try:
@@ -104,25 +119,25 @@ class EsposallesDataset():
             return xout,np.asarray(Y),current_example
 
         else:
-            self.r_id=self.register_iterator.next()
             self.word_iterator=iter(self.reg_dict[self.r_id])
-            example_seq=[]
-            labels_seq=[]
-            words_seq=[]
-            for word in self.word_iterator:
-                X=readNormalizedImage(word)
-                #if X.shape[-2] >= miny and X.shape[-1] >= minx:
-                #   #print current_example,self.labels[current_example],X.shape[-2],X.shape[-1]
-                #   break;
-                #print 'Image too small: ',current_example,self.labels[current_example],X.shape[-2],X.shape[-1]
-                Y=[self.labeldict[self.labels[word]]]
-                xout=np.asarray(X)[np.newaxis,np.newaxis,:,:]
-                if K.backend()=='tensorflow':
-                    xout=xout[0,:,:,:,np.newaxis]
-                example_seq.append(xout)
-                labels_seq.append(Y)
-                words_seq.append(word)
-            return example_seq,labels_seq,words_seq
+            if K.backend()=='tensorflow':
+                X=[np.asarray(self.readNormalizedImage(i))[:,:,np.newaxis] for i in self.word_iterator]
+            else:
+                X=[np.asarray(self.readNormalizedImage(i))[np.newaxis,:,:] for i in self.word_iterator]
+            self.word_iterator=iter(self.reg_dict[self.r_id])
+            Y=[np.asarray(self.labeldict[self.labels[i]]) for i in self.word_iterator]
+            self.word_iterator=iter(self.reg_dict[self.r_id])
+            IDS=[i for i in self.word_iterator]
+            try:
+                self.r_id=self.register_iterator.next()
+            except StopIteration: #if no more register, shuffle and get first register again
+                np.random.shuffle(self.shuffled_registers)
+                self.register_iterator=iter(self.shuffled_registers)
+                self.r_id=self.register_iterator.next()
+                self.epoch+=1
+            print X[0].shape
+            return X,Y,IDS
+
 
     def get_transcription_from_categorical(self,predictions):
         if self.revdict is None: self.revdict={v:k for (k,v) in self.labeldict.iteritems()}
@@ -132,20 +147,17 @@ class EsposallesDataset():
 
 def test_input():
     ims,labs,names=EsposallesDataset(cvset='train',level='sequence').get_example()
-    print EsposallesDataset(cvset='train').labeldict
     widths=[]
     heights=[]
     for i in range(len(ims)):
-
         im=ims[i]
-        heights.append(im.shape[1])
-        widths.append(im.shape[2])
-        im=im*255
-        im=im.astype('uint8')
-        im=im[0][:,:,0]
-        print labs[i],names[i]
 
-        im=Image.fromarray(im)
+        heights.append(im.shape[0])
+        widths.append(im.shape[1])
+        #im=im*255
+        im=im.astype('uint8')
+        print labs[i],names[i],im.shape
+        im=Image.fromarray(im[:,:,0])
         im.show()
     print np.mean(heights)
 
