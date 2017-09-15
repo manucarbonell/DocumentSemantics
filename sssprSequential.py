4ec63cdf22efc3437ac5f0a55971a8d220ad34f8
manucarbonell@github.com <manucarbonell@github.com>
2017-09-15 10:44:50 +0200

    Remove verboses

--- sssprSequential.py
+++ sssprSequential.py
@@ -22,6 +22,7 @@ experiment_id=os.path.splitext(__file__)[-1]
 max_non_improving_epochs=config.max_non_improving_epochs
 min_epochs=config.min_epochs
 verbose_period=config.verbose_period
+
 def buildModel():
     inputimages = Input(shape=(None,config.im_height,config.im_width, config.im_depth))
 
@@ -33,11 +34,11 @@ def buildModel():
     x=Conv2D(64,(3,3),padding='same',activation='relu')(x)
     x=Conv2D(64,(3,3),padding='same',activation='relu')(x)
     x=MaxPooling2D(pool_size=(2,2))(x)
-    #x=Conv2D(128,(3,3),padding='same',activation='relu')(x)
-    #x=Conv2D(128,(3,3),padding='same',activation='relu')(x)
+    x=Conv2D(128,(3,3),padding='same',activation='relu')(x)
+    x=Conv2D(128,(3,3),padding='same',activation='relu')(x)
     x=Conv2D(256,(3,3),padding='same',activation='relu')(x)
     x=SpatialPyramidPooling([1, 2, 4])(x)
-    #x=Dense(2048,activation='relu')(x)
+    x=Dense(2048,activation='relu')(x)
     x=Dropout(0.5)(x)
     x=Dense(512,activation='relu')(x)
     x=Dropout(0.5)(x)
@@ -49,7 +50,7 @@ def buildModel():
     x2=Bidirectional(LSTM(100,return_sequences=True,dropout=0.5,recurrent_dropout=0.5))(x2)
 
     outputs=TimeDistributed(Dense(6,activation='softmax'))(x2)
-    optimizer=SGD(lr=0.0001,momentum=0.9,nesterov=True,decay=0.000001)
+    optimizer=SGD(lr=config.learning_rate,momentum=0.9,nesterov=True,decay=config.lr_decay)
     m=Model(inputs=[inputimages],outputs=[outputs])
     m.compile(loss='categorical_crossentropy',optimizer=optimizer,metrics=['acc'])
 
@@ -62,19 +63,6 @@ def smoothlabel(x,amount=0.25,variance=5):
     smoothed=x*(1-noise.sum())+noise
     return smoothed
 
-def noisylabel(x,prob=0.3):
-    if np.random.random()<prob:
-       return np.random.permutation(x)
-    return x
-
-def getPreviousLabel(example,previous_prediction,true_previous_label,dataset):
-    new_register=np.asarray([[0.,0.,0.,0.,0.,0.,1.]],dtype='float32')
-    previous_label=np_utils.to_categorical([dataset.prevlabels[example]],7)
-    if true_previous_label or (previous_label==new_register).all():
-        return previous_label
-    if previous_prediction.size==7:
-        return previous_prediction
-    return np.hstack((previous_prediction==np.max(previous_prediction),[[0.]]))
 
 def trainModel(m):
     print "Train model..."
@@ -95,14 +83,13 @@ def trainModel(m):
         for j in range(E.epoch_size/config.batch_size):
 
             x,y,example_id=E.get_batch()
+            #E.show_batch()
 
             #y=smoothlabel(y)
 
             l,a=m.train_on_batch([x],y)#,class_weight=E.class_weights)
 
-            if j % verbose_period == 0 and j>0:
-                print "Epoch",epoch,"step",j," loss ",np.sum(losses[j-verbose_period:j])
-
+           
             accs.append(a)
             losses.append(l)
 
@@ -112,7 +99,7 @@ def trainModel(m):
         for jv in range(E.epoch_size/config.batch_size):
 
             x,y,example_id=Ev.get_batch()
-            print "validation..."
+            print "\r Evaluating on validation set...",1+jv*100/(E.epoch_size/config.batch_size),"%",
             #y=smoothlabel(y)
             l,a=m.evaluate([x],y,verbose=0)
             accs.append(a)
@@ -149,7 +136,6 @@ def evaluateModel(m,show_confmat=False):
         print "%s,%s"% (example_id,revdict[y_pred])#,revdict[y[0]]
         confmat[np.argmax(predicted_label),y[0]]+=1
         confmat[y_pred,y[0]]+=1
-        y=np_utils.to_categorical(y,6)
         l,a=m.evaluate([x],y,verbose=0)
         accs.append(a)
         losses.append(l)
@@ -164,6 +150,7 @@ def main():
 
     trainModel(m)
     print "Training finished."
+
     print "Testing model..."
     evaluateModel(m,true_previous_label=True)
     #
