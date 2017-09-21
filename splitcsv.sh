75b6eadb464b7ebc135142cbd78ce11909c43a5a
Juan Ignacio Toledo <juanignacio.toledo@scytl.com>
2017-09-21 12:16:52 +0200

    Small fixes and variable name changes

--- splitcsv.sh
+++ splitcsv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-join -j 1 -t, transcription.csv $1 > ${1}_full_output.csv
+join -j 1 -t, transcription.csv $1 > tmpfile1_$$
+grep -v ',other,none$' tmpfile1_$$ > tmpfile2_$$ 
+grep -v ',other$' tmpfile2_$$ > ${1%.*}_full_output.csv 
 OUTDIR=split_${1%.*}
 mkdir ${OUTDIR}; 
-for record in $(cut -d_ -f1,2 ${1}_full_output.csv  | sort -Vu); do 
+for record in $(cut -d_ -f1,2 ${1%.*}_full_output.csv  | sort -Vu); do 
 	echo $record;
-	grep ^${record}_ ${1}_full_output.csv > ${OUTDIR}/${record}_output.csv; 
+	grep ^${record}_ ${1%.*}_full_output.csv > ${OUTDIR}/${record}_output.csv; 
 done 
+rm  tmpfile1_$$ tmpfile2_$$ ${1%.*}_full_output.csv 
